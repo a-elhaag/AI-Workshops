c5a0f53bce78053de6149e4fabd2e9ccdc3767cb
Anas Elhaag <anaselhaag@gmail.com>
2024-07-23 23:21:40 +0300

    chore: Add rule-based chatbot implementation and keyword responses

---
+++ Week_1/Chatbot/Keywords.py
@@ -0,0 +1,50 @@
+keywords_responses = { 
+    'greetings': { #category
+        'keywords': ['hello' , 'hi', 'hey', 'greetings'],  # data
+        'responses': ['Hello!', 'Hi there!', 'Hey!', 'Greetings!']
+    },
+    'farewells': {  #category
+        'keywords': ['bye', 'goodbye', 'see you', 'take care'],
+        'responses': ['Goodbye!', 'See you later!', 'Take care!', 'Bye!']
+    },
+    'age': {    #category
+        'keywords': ['old', 'age'],
+        'responses': ['I am a computer program, I do not have an age!']
+    },
+    'name': {       #category
+        'keywords': ['name', 'call'],
+        'responses': ['You can call me Alex.', 'I am Alex!']
+    },
+    'weather': {
+        'keywords': ['weather'],
+        'responses': ['I am a computer program, I do not have the ability to check the weather!']
+    },
+    'music': {
+        'keywords': ['music', 'song', 'songs'],
+        'responses': ['I love music!']
+    },
+    'movies': {
+        'keywords': ['movie', 'movies', 'film', 'films'],
+        'responses': ['I love watching movies!']
+    },
+    'food': {
+        'keywords': ['food', 'foods', 'eat', 'eating'],
+        'responses': ['I love food!']
+    },
+    'hobbies': {
+        'keywords': ['hobby', 'hobbies'],
+        'responses': ['I have many hiking!']
+    },
+    'help': {
+        'keywords': ['help', 'support'],
+        'responses': ['I am here to help you!']
+    },
+    'thanks': {
+        'keywords': ['thank', 'thanks'],
+        'responses': ['You are welcome!', 'No problem!', 'My pleasure!']
+    },
+    'default': {
+        'keywords': [],
+        'responses': ['I am sorry, I do not understand.', 'I am not sure how to respond to that.', 'Can you provide more information?']
+    }
+}
---
+++ Week_1/Chatbot/Rule_based_chatbot.py
@@ -0,0 +1,25 @@
+import random
+
+import Keywords
+
+
+def main():
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == "exit": # lower is used to convert the input to lowercase
+            print("Chatbot: Goodbye!")
+            break
+        response = chatbot(user_input)
+        print("Chatbot:", response)
+
+
+def chatbot(user_input):
+    for category, data in Keywords.keywords_responses.items():
+        for keyword in data["keywords"]:
+            if keyword in user_input.lower():
+                return random.choice(data["responses"])
+    return "I'm not sure how to respond to that."
+
+
+if __name__ == "__main__":
+    main()
